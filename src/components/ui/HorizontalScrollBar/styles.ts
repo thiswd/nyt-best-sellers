c4e2bfc82890585fcdf0491a371307a6c48e5787
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-31 14:46:30 -0300

    style(HorizontalScrollBar): increase scrollbar border-radius 🎨
    Increased the 'border-radius' of the scrollbar in the 'HorizontalScrollBar' component. This adjustment makes the scrollbar appear more rounded, improving the visual aesthetics of the component.

--- src/components/ui/HorizontalScrollBar/styles.ts
+++ src/components/ui/HorizontalScrollBar/styles.ts
@@ -6,7 +6,7 @@ export const ScrollBarContainer = styled.div`
   ::-webkit-scrollbar {
     height: 0.75rem;
     background: rgba(153, 27, 27, 0.9);
-    border-radius: 0.12rem;
+    border-radius: 0.25rem;
   }
 
   ::-webkit-scrollbar-thumb {
