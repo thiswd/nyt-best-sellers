7b08ebcce50c4aea26920ecf094d4ca0917171e5
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-28 23:03:01 -0300

    styles: adjust title margins

--- src/components/MainTitle/styles.ts
+++ src/components/MainTitle/styles.ts
@@ -2,19 +2,31 @@ import styled from "styled-components"
 import { media } from "../../styles/screenSizes"
 
 export const MainTitleContainer = styled.div`
-  margin: 2rem 2rem 1.5rem;
+  margin: 1.5rem 1rem 1rem;
+
+  ${media.lg`
+    margin: 1.5rem 1.75rem 1rem;
+  `}
 `
 
 export const StyledTitle = styled.h1`
   font-family: var(--font-header);
   font-size: 2.5rem;
 
+  ${media.sm`
+    font-size: 2.4rem;
+  `}
+
   ${media.md`
-    font-size: 2.9rem;
+    font-size: 2.7rem;
+  `}
+
+  ${media.lg`
+    font-size: 3.2rem;
   `}
 
   ${media.xl`
-    font-size: 3.2rem;
+    font-size: 3.4rem;
   `}
 `
 
@@ -26,13 +38,19 @@ export const SectionName = styled.div`
     font-family: var(--font-subtitle);
     font-weight: 500;
     font-size: 0.8rem;
+    margin-bottom: 0.15rem;
 
     ${media.md`
-      font-size: 0.9rem;
+      font-size: 0.8rem;
+    `}
+
+    ${media.lg`
+      font-size: 1rem;
+      margin-bottom: 0;
     `}
 
     ${media.xl`
-      font-size: 1rem;
+      font-size: 1.1rem;
     `}
   }
 
