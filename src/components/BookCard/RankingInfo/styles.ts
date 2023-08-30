e631c9528a45844e0248928cc89a62519efe55d5
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-29 23:48:42 -0300

    style: set min-height for book ranking container

--- src/components/BookCard/RankingInfo/styles.ts
+++ src/components/BookCard/RankingInfo/styles.ts
@@ -4,7 +4,8 @@ import styled from "styled-components"
 
 export const RankingContainer = styled.div`
   width: 100%;
-  height: 80px;
+  height: 5rem;
+  min-height: 5rem;
   display: flex;
   align-items: center;
   justify-content: center;
