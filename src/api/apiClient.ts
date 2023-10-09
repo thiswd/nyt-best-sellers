6c4f11b02cc2ed91b99faa59626f71ab3b6cebee
Thiago Sawada <tms1_tms2@hotmail.com>
2023-10-08 23:29:34 -0300

    refactor(api): adjust base URL for NYT Books API
    - Modified the API_BASE_URL to a more general endpoint for better flexibility in API requests.

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://api.nytimes.com/svc/books/v3/lists/current"
+const API_BASE_URL = "https://api.nytimes.com/svc/books/v3/lists/"
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
